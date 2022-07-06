51be535d33ed90be6799a736c91e44b55b36d2e5
stamp222 <jacekszyper@gmail.com>
2022-07-06 11:08:24 +0200

    Initial commit

---
+++ src/test/java/com/example/mvcrest/MvcRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvcrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
